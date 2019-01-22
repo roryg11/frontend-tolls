c6ba056da0be7e1e6852f2c4ec6ef9ef315d42f1
Rory Grant <Rory.C.Grant@gmail.com>
2019-01-22 12:02:57 -0800

    adding styled components and server side initial rendering

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,11 +1,27 @@
 import Link from "next/link";
+import NavStyles from "./styles/NavStyles";
 
 const Nav = () => (
-    <div>
+    <NavStyles>
+        <Link href="/items">
+            <a>Items</a>
+        </Link>
         <Link href="sell">
             <a>Sell</a>
         </Link>
-    </div>
+        <Link href="/signup">
+            <a>Signup</a>
+        </Link>
+        <Link href="/orders">
+            <a>Orders</a>
+        </Link>
+        <Link href="/me">
+            <a>Account</a>
+        </Link>
+        <Link href="/">
+            <a>Home</a>
+        </Link>
+    </NavStyles>
 )
 
 export default Nav; 
