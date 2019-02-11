d3dd636658d69db6a67fe3071695dc148a6dd181
Rory Grant <Rory.C.Grant@gmail.com>
2019-02-10 18:52:15 -0800

    creating gateway component

---
+++ sick-fits/frontend/components/PleaseSignin.js
@@ -0,0 +1,23 @@
+import { CURRENT_USER_QUERY } from "./User";
+import { Query } from "react-apollo";
+import SigninForm from "./SigninForm";
+
+const PleaseSignin = props => (
+    <Query query={CURRENT_USER_QUERY}>
+        {
+            (data, loading)=>{
+                if(loading) return <p>Loading...</p>
+                if(!data.me){
+                    return <div>
+                            <p>Please sign in before continuing</p>
+                            <SigninForm/>
+                        </div>
+                }
+
+                return props.children
+            }
+        }
+    </Query>
+)
+
+export default PleaseSignin;
