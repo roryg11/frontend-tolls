2eac8ea843bd2b99c142e949af26b49e31ec6b0c
Rory Grant <Rory.C.Grant@gmail.com>
2019-02-11 16:21:54 -0800

    adding permissions UI

--- sick-fits/frontend/components/PermissionsTable.js
+++ sick-fits/frontend/components/PermissionsTable.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import gql from "graphql-tag";
-import { Query } from "react-apollo";
+import { Query, Mutation } from "react-apollo";
 import Error from "./ErrorMessage";
 import Table from "./styles/Table";
 import SickButton from "./styles/SickButton";
+import PropTypes from "prop-types";
 
 const ALL_USERS_QUERY = gql`
     query ALL_USERS_QUERY {
@@ -25,16 +26,43 @@ const permissions = [
     "PERMISSIONUPDATE"
 ];
 
-class User extends Component {
+class UserPermission extends Component {
+    static propTypes = {
+        user: PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            name: PropTypes.string.isRequired,
+            email: PropTypes.string.isRequired,
+            permissions: PropTypes.array.isRequired
+        }).isRequired
+    }
+
+    state = {
+        name: this.props.user.name,
+        email: this.props.user.email,
+        permissions: this.props.user.permissions
+    }
+
+    onCheckBoxChange = e => {
+        let updatedPermissions = [...this.state.permissions];
+        if(e.target.checked){
+            updatedPermissions.push(e.target.value);
+            this.setState({permissions: updatedPermissions});
+        } else {
+            const indexOfPerm = updatedPermissions.indexOf(e.target.value);
+            updatedPermissions = updatedPermissions.splice(1, indexOfPerm);
+            this.setState({permissions: updatedPermissions});
+        }
+    }
+
     render(){
         const user = this.props.user;
         return (<tr key={user.id}>
                     <td>{user.name}</td>
                     <td>{user.email}</td>
                     { permissions.map((userPermission) => {
-                        return (<td>
-                                    <label htmlFor={`${user.id}-${userPermission}`}>
-                                        <input type="checkbox"/>
+                        return (<td key={userPermission}>
+                                    <label htmlFor={`${user.id}-permission-${userPermission}`}>
+                                        <input type="checkbox" value={userPermission} onChange={this.onCheckBoxChange}/>
                                     </label>
                                 </td>);
                     })}
@@ -67,7 +95,7 @@ class PermissionsTable extends Component {
                             <tbody>
                                 {
                                     users.map((user)=>{
-                                        return (<User user={user} key={user.id}/>);
+                                        return (<UserPermission user={user} key={user.id}/>);
                                     })
                                 }
                             </tbody>
